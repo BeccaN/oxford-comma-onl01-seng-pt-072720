a42c4b6a419c64d14fbf7773219d1991e3d6eef7
Rebecca Congi <rncongi@gmail.com>
2020-08-02 23:04:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array[0..1].join(", ") + ", and " + array[-1]
     
   elsif array.length > 3
-    array[0...-2].join(", ") + ", and" + array[-1]
+    array[0...-1].join(", ") + ", and " + array[-1]
     
   else
     array.join(", ")
